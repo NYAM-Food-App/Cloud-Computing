1bcde45de2baea1d0d4e93729c34fab01f469a03
AqilMF <125326657+mofach@users.noreply.github.com>
2024-12-03 16:46:19 +0700

    Delete src directory

--- src/routes/foodRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { chooseFood } = require('../controllers/foodController');
-
-const router = express.Router();
-
-// Endpoint untuk memilih makanan
-router.post('/:uid', chooseFood);
-
-module.exports = router;
